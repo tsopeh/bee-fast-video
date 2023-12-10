bd8e4367b431d2dbaa750f859e24cea136db5acb
Petar Stokić <5ar100kic@gmail.com>
2023-12-10 23:41:58 +0100

    better position calculation

--- src/content/controller.tsx
+++ src/content/controller.tsx
@@ -9,11 +9,10 @@ export const Controller = ({ videoEl }: Props) => {
   const [isPaused, setIsPaused] = useState(videoEl.paused)
   const [isVideoInViewport, setIsVideoInViewport] = useState(false)
   const [zIndex, setZIndex] = useState<number>(() => {
-    return getZIndexOfElement(videoEl) + 1
+    return getElementZIndex(videoEl) + 1
   })
   const [position, setPosition] = useState(() => {
-    const { x, y } = videoEl.getBoundingClientRect()
-    return { left: x, top: y }
+    return getElementPosition(videoEl)
   })
 
   useEffect(() => {
@@ -22,13 +21,7 @@ export const Controller = ({ videoEl }: Props) => {
       setIsPaused(videoEl.paused)
     }
     const updatePosition = () => {
-      const { x, y } = videoEl.getBoundingClientRect()
-      setPosition(prevPosition => {
-        const didChangePosition = prevPosition.left != x || prevPosition.top != y
-        return didChangePosition
-          ? { left: x, top: y }
-          : prevPosition
-      })
+      setPosition(getElementPosition(videoEl))
     }
     videoEl.addEventListener("play", onPlayPauseChange)
     videoEl.addEventListener("pause", onPlayPauseChange)
@@ -41,7 +34,7 @@ export const Controller = ({ videoEl }: Props) => {
     // Track position change
     const styleMutationObserver = new MutationObserver(() => {
       updatePosition()
-      setZIndex(getZIndexOfElement(videoEl) + 1)
+      setZIndex(getElementZIndex(videoEl) + 1)
     })
     styleMutationObserver.observe(videoEl, { childList: false, attributes: true, attributeFilter: ["style"] })
 
@@ -72,7 +65,15 @@ export const Controller = ({ videoEl }: Props) => {
   </div>
 }
 
-function getZIndexOfElement (element: HTMLElement): number {
+function getElementZIndex (element: HTMLElement): number {
   const parsed = Math.floor(parseFloat(window.getComputedStyle(element).getPropertyValue("z-index")))
   return Number.isNaN(parsed) ? 0 : parsed
+}
+
+function getElementPosition (element: HTMLElement): { top: number, left: number } {
+  const { x, y } = element.getBoundingClientRect()
+  return {
+    left: x + window.scrollX,
+    top: y + window.screenY,
+  }
 }
