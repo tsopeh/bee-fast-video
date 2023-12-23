a9c26607cf5000b3aabd33eeaca412f7a11a860a
Petar Stokić <5ar100kic@gmail.com>
2023-12-23 16:06:37 +0100

    do not ron on pages without html document; empty popup

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,9 +1,11 @@
 import { h, render } from "preact"
 import { ControllersContainer } from "./controllers-container"
 
-const rootElement = document.createElement("div")
-rootElement.classList.add("bee-fast-video-root-element")
-document.body.appendChild(rootElement)
+const isValidHtmlDocument = document.body != null
+if (isValidHtmlDocument) {
+  const rootElement = document.createElement("div")
+  rootElement.classList.add("bee-fast-video-root-element")
+  document.body.appendChild(rootElement)
 
-render(h(ControllersContainer, null), rootElement)
-
+  render(h(ControllersContainer, null), rootElement)
+}
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,27 +1,10 @@
-import { Logo } from "../assets/img/logo"
 import popupCss from "./popup.scss?inline"
 
 export const Popup = () => {
   return (
     <>
       <style>{popupCss}</style>
-      <div class="w-full bg-[#673ab8] p-8 text-center text-lg">
-        <Logo />
-        <p class="text-white">Hello Vite + Preact!</p>
-        <p class="text-white">
-          <a
-            class="border-b-2"
-            href="https://preactjs.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Preact
-          </a>
-        </p>
-        <p data-testid="popup_text" class="p-6 text-3xl text-purple-400">
-          Pop up page
-        </p>
-      </div>
+      <div>Bee fast video</div>
     </>
   )
 }
